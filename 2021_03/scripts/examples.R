a5436fc062ffb50bc4a515e0a07faa6f702f38eb
Ankur Ankan <ankurankan@gmail.com>
2021-03-22 14:48:26 +0100

    Updates slides

--- 2021_03/scripts/examples.R
+++ 2021_03/scripts/examples.R
@@ -27,28 +27,28 @@ x ~ u + z
 y ~ u + x + w
 "
 
-m_2 <- "
-u1 =~ z2 + z1 + x + y
-u2 =~ w + x + y
-z1 ~ u1 + z2
-x ~ u1 + u2 + z1
-y ~ u1 + u2 + x + w
-u1 ~~ u2
-"
+# m_2 <- "
+# u1 =~ z2 + z1 + x + y
+# u2 =~ w + x + y
+# z1 ~ u1 + z2
+# x ~ u1 + u2 + z1
+# y ~ u1 + u2 + x + w
+# u1 ~~ u2
+# "
+# 
+# m_3 <- "
+# u1 =~ z1 + z2 + x + y
+# u2 =~ w1 + w2 + x + y
+# z2 ~ u1 + w2
+# x ~ u1 + u2 + z1
+# y ~ u1 + u2 + x
+# u1 ~~ u2
+# "
 
-m_3 <- "
-u1 =~ z1 + z2 + x + y
-u2 =~ w1 + w2 + x + y
-z2 ~ u1 + w2
-x ~ u1 + u2 + z1
-y ~ u1 + u2 + x
-u1 ~~ u2
-"
-
-models <- list(m_a, m_b, m_c, m_d, m_2, m_3)
+models <- list(m_b, m_c, m_d)
 
 for (i in 1:length(models)){
     print(miivs(models[[i]]))
-    # d <- simulateSEM(lavaanToGraph(models[[i]]), b.default=0.4, N=1e5)
+    d <- simulateSEM(lavaanToGraph(models[[i]]), b.default=0.4, N=1e5)
     # print(miive(models[[i]], d))
 }
