459bfb26324ff9889fbef8e634836ae0b1ecc9f0
Ankur Ankan <ankurankan@gmail.com>
2021-07-13 19:21:17 +0200

    Updates

--- 2021_07_conditional_independence/scripts/models.py
+++ 2021_07_conditional_independence/scripts/models.py
@@ -1,21 +1,25 @@
-from pgmpy.models import BayesianModel
+import daft
 import matplotlib.pyplot as plt
 
-example_dag = BayesianModel(
-    [
-        ("Pollution", "Cancer"),
-        ("Smoker", "Cancer"),
-        ("Cancer", "XRay"),
-        ("Cancer", "Breathing"),
-    ]
-)
-p = example_dag.to_daft(
-    node_pos={
-        "Pollution": (-1, 1),
-        "Smoker": (1, 1),
-        "Cancer": (0, 0),
-        "XRay": (-1, -1),
-        "Breathing": (1, -1),
-    }
-).render()
-plt.savefig("../imgs/example_dag.png")
+# example_dag = BayesianModel(
+#     [
+#         ("Pollution", "Cancer"),
+#         ("Smoker", "Cancer"),
+#         ("Cancer", "XRay"),
+#         ("Cancer", "Breathing"),
+#     ]
+# )
+
+pgm = daft.PGM(aspect=1)
+pgm.add_node("pollution", r"Pollution", 0.5, 1.5)
+pgm.add_node("smoker", r"Smoker", 1.5, 1.5)
+pgm.add_node("cancer", r"Cancer", 1, 1)
+pgm.add_node("xray", r"XRay", 0.5, 0.5)
+pgm.add_node("breathing", r"Breathing", 1.5, 0.5)
+
+pgm.add_edge('pollution', 'cancer')
+pgm.add_edge('smoker', 'cancer')
+pgm.add_edge('cancer', 'xray')
+pgm.add_edge('cancer', 'breathing')
+
+pgm.savefig("../imgs/example_dag.png")
