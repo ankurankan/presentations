84725f0250a45b9592716508c9746b681772b7c1
Ankur Ankan <ankurankan@gmail.com>
2021-09-03 13:47:57 +0200

    Updates

--- 2021_07_conditional_independence/scripts/models.py
+++ 2021_07_conditional_independence/scripts/models.py
@@ -23,3 +23,18 @@ pgm.add_edge('cancer', 'xray')
 pgm.add_edge('cancer', 'breathing')
 
 pgm.savefig("../imgs/example_dag.png")
+
+
+## Model example structure learning
+pgm1 = daft.PGM(aspect=1)
+pgm1.add_node("pollution", r"Pollution", 0.5, 1.5)
+pgm1.add_node("smoker", r"Smoker", 1.5, 1.5)
+pgm1.add_node("cancer", r"Cancer", 1, 1)
+pgm1.add_node("xray", r"XRay", 0.5, 0.5)
+pgm1.add_node("breathing", r"Breathing", 1.5, 0.5)
+
+pgm1.add_edge('breathing', 'cancer', directed=False)
+pgm1.add_edge('pollution', 'cancer', directed=False)
+pgm1.add_edge('smoker', 'cancer', directed=False)
+
+pgm1.savefig("../imgs/example_sl.png")
